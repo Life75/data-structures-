d126e825e05d124ced9606101b7aa897023f9b1e
Life75 <austynwashington@yahoo.com>
2022-09-18 12:30:23 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+</script>
+
+<template>
+<!--TODO add git control and ElementPlus UI as well -->
+<RouterView></RouterView>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="homepage">
+This is the home page 
+
+</div>
+</template>
+
+
+<script setup lang="ts">
+
+</script> 
+
+<style>
+
+</style>
