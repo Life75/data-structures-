2a17e1d97cd9076155252d8fb1da98cb256c8945
Life75 <austynwashington@yahoo.com>
2023-10-25 18:25:29 -0400

    working on fade in transition

--- src/components/NavigationHeaderMobile.vue
+++ src/components/NavigationHeaderMobile.vue
@@ -2,7 +2,7 @@
 <div>
 <ul class="flex justify-between space-x-10 pt-8">
   <li class="">
-    <el-icon :size="24" @click="openDrawer()"><Menu /></el-icon>
+    <el-icon :size="24" @click="drawer = true"><Menu /></el-icon>
   </li>
   <li class=" justify-center">
   </li>
@@ -11,21 +11,72 @@
   </li>
 </ul>
 <el-drawer v-model="drawer" title="I am the title" :with-header="false" size="70%" direction="ltr" custom-class="bg-neutral-950">
-    <span>Hi there!</span>
+    <li v-for="(item, index) in list">
+        <span :class="item">{{ index }} {{ item }}</span>
+    </li>
     <!--Here we work and place the animation for the routes to follow through as well-->
   </el-drawer>
 </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-var drawer = ref(false)
-
-function openDrawer() {
-  console.log('open :D')
-  drawer.value = true
+import { ref, watch, Ref } from 'vue';
+interface classList {
+  data: any
+  className?: String 
 }
 
+var drawer = ref(false)
+var list: Ref<Array<classList>> = ref([{
+  data: 1,
+  className: "n"
+},
+{
+  data: 2,
+  className: "n"
+}, 
+{
+  data: 3, 
+  className: "n"
+}
+])
+var testList = ref(["a", "b", "c"])
+
+watch(drawer, (before, after) => {
+  if(drawer.value) {
+    for (let i = 0; i < testList.value.length; ++i) {
+      fadeIn(testList.value[i], i * 1000)
+    }
+    function fadeIn (item, delay) {
+      setTimeout(() => {
+        item = ('fadein')
+      }, delay)
+    }
+  }
+})
+
+
+
+//example: https://stackoverflow.com/questions/50271541/how-can-i-animate-list-items-to-be-apeared-one-by-one-with-javascript
 </script>
 
-<style></style>
+<style>
+.fade-item {
+    transition: .2s all ease-in-out;
+    opacity: 0;
+}
+
+.fadein {
+    animation: fadeIn 0.9s 1;
+    animation-fill-mode: forwards;
+}
+
+@keyframes fadeIn {
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+}
+</style>
