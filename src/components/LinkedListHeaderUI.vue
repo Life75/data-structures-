c1fe75f6badabd63cab999a6134ef26d787a69d0
Life75 <austynwashington@yahoo.com>
2024-07-24 22:48:27 -0400

    working on header overall

--- src/components/LinkedListHeaderUI.vue
+++ src/components/LinkedListHeaderUI.vue
@@ -1,6 +1,6 @@
 <template>
   <div ref="linked-list-header"
-    class="flex flex-col md:flex-row gap-5 mt-5  justify-center align-middle md:items-stretch items-center">
+    class="flex flex-col md:flex-row gap-5 mt-5 mx-4  justify-center align-middle md:items-stretch items-center">
     <div class="flex flex-col items-stretch w-full gap-2 uppercase">
       <span class="flex w-full h-full ">
         <div class=" flex justify-center  items-center grow text-center  bentoStyling ">Pop</div>
@@ -14,52 +14,28 @@
     </div>
 
     <div class="bentoStyling">
-      <div class="mx-5 mt-5 gap-2 flex flex-col">
-        <p class="text-lg uppercase">Complexity</p>
+      <div class="mx-5 mt-5 gap-2 flex flex-col mb-5 ">
+        <p class="text-lg uppercase">Controls</p>
+        <div class="flex flex-col gap-2 align-middle ">
+          <p class="w-full">Next value to push</p>
+          <input type="text" placeholder="" class="input  w-full max-w-xs" />
 
-        <span class="flex flex-row align-middle items-center text-4xl gap-2">
-          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-clock"
-            viewBox="0 0 16 16">
-            <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
-            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
-          </svg>
-          <p class="uppercase">{{ props.sortRequestController?.metadata.timeComplexity }}</p>
-        </span>
+        </div>
+        <div class="flex flex-col gap-2 align-middle">
+          <p class="w-full">Value to seek</p>
+          <input type="text" placeholder="" class="input  w-full max-w-xs" />
 
-
-        <span class="flex flex-row align-middle items-center text-4xl gap-2">
-          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-memory"
-            viewBox="0 0 16 16">
-            <path
-              d="M1 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.586a1 1 0 0 0 .707-.293l.353-.353a.5.5 0 0 1 .708 0l.353.353a1 1 0 0 0 .707.293H15a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zm.5 1h3a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5m5 0h3a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5m4.5.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5zM2 10v2H1v-2zm2 0v2H3v-2zm2 0v2H5v-2zm3 0v2H8v-2zm2 0v2h-1v-2zm2 0v2h-1v-2zm2 0v2h-1v-2z" />
-          </svg>
-          <p class="uppercase">{{ props.sortRequestController?.metadata.spaceComplexity }}</p>
-        </span>
+        </div>
       </div>
     </div>
 
     <div class="bentoStyling">
       <div class="mx-5 mt-5 gap-2 flex flex-col">
-        <p class="text-lg uppercase">Complexity</p>
+        <p class="text-lg uppercase">Settings</p>
+        <p>Animation Speed </p>
+        <v-range-slider></v-range-slider>
 
-        <span class="flex flex-row align-middle items-center text-4xl gap-2">
-          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-clock"
-            viewBox="0 0 16 16">
-            <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
-            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
-          </svg>
-          <p class="uppercase">{{ props.sortRequestController?.metadata.timeComplexity }}</p>
-        </span>
-
-
-        <span class="flex flex-row align-middle items-center text-4xl gap-2">
-          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-memory"
-            viewBox="0 0 16 16">
-            <path
-              d="M1 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.586a1 1 0 0 0 .707-.293l.353-.353a.5.5 0 0 1 .708 0l.353.353a1 1 0 0 0 .707.293H15a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zm.5 1h3a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5m5 0h3a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5m4.5.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5zM2 10v2H1v-2zm2 0v2H3v-2zm2 0v2H5v-2zm3 0v2H8v-2zm2 0v2h-1v-2zm2 0v2h-1v-2zm2 0v2h-1v-2z" />
-          </svg>
-          <p class="uppercase">{{ props.sortRequestController?.metadata.spaceComplexity }}</p>
-        </span>
+        
       </div>
     </div>
 
