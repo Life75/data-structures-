5efe3e9c762ad851b4198b998650797e843aa84a
awashington <awashington@healthqlix.com>
2024-12-25 22:34:56 -0500

    button change with better look

--- src/Views/VSorting.vue
+++ src/Views/VSorting.vue
@@ -9,15 +9,16 @@
         </span>
         <span class="md:hidden mt-5 mx-5 ml-auto">
             <span @click="drawerIsOpen = !drawerIsOpen">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="size-9">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />
-                </svg>
-
+                <button id="mobile-drawer" 
+                    class="btn rounded-full w-14 h-14 bg-neutral-800/70 backdrop-blur-lg border border-neutral-700/50 hover:bg-neutral-700/70 shadow-lg flex items-center justify-center"> 
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-7 text-green-400">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />
+                    </svg>
+                </button>
             </span>
-        </span>
-        <RouterView class="flex h-[500px] my-10" @request="setSortRequest" :sortProps="sortPropsRef"
+        </span>        <RouterView class="flex h-[500px] my-10" @request="setSortRequest" :sortProps="sortPropsRef"
             :animationSpeed="animeSpeed" @timer="setTimer">
           </RouterView>
           <div class="backdrop-blur-md bg-base-300/30 rounded-lg p-6 border border-neutral-700/50 hover:border-green-400 transition-all shadow-lg">
@@ -99,3 +100,7 @@ function setTimer(timer: Timer): void {
     trackTime.value = timer;
 }
 </script>
+
+<style>
+
+</style>
